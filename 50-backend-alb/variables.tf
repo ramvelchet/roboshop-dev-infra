3ed5da9f03b72b5f6e76c7c80529a1a7e3481874
ramvelcheti <ramwalfox@gmail.com>
2025-12-26 16:54:46 +0530

    terraform

---
+++ 50-backend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z0877618Q6W0PB02GEO4"
+}
+
+variable "domain_name" {
+    default = "ramvelcheti.store"
+}
---
+++ 70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+
+}
+variable "environment" {
+    default = "dev"  
+}
+
+variable "domain_name" {
+    default = "ramvelcheti.store"
+}
+
+variable "zone_id" {
+    default = "Z0877618Q6W0PB02GEO4"
+}
